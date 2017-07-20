63e5718fa1811c93bbd78e2a9b65f0a2de7d97a8
RoryEnright <enright2062@gmail.com>
2017-07-20 16:00:22 -0400

    Bubble up DELETE through app structure
    Delete is now working as intended.

--- app/components/mountain-hike.js
+++ app/components/mountain-hike.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteHike () {
+      console.log('hello')
+      return this.sendAction("deleteHike", this.get('hike'));
+    },
+  }
 });
--- app/routes/mountain.js
+++ app/routes/mountain.js
@@ -5,4 +5,13 @@ export default Ember.Route.extend({
     // params object and mountain_id from that param
     return this.get('store').findRecord('mountain', params.mountain_id);
   },
+  actions: {
+    deleteHike (hike) {
+      console.log('mountain')
+    hike.destroyRecord();
+   },
+  // deleteHike: function() {
+  //   this.get('model.hike').destroyRecord();
+  // }
+  }
 });
