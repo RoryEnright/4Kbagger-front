feaf8e262dad50bb2b07df80397051b7de5ffd29
RoryEnright <enright2062@gmail.com>
2017-07-24 00:37:01 -0400

    Add transition to mountains page after add/edit
    Page now takes you back to the mountains page

--- app/components/hike-form-add.js
+++ app/components/hike-form-add.js
@@ -10,41 +10,33 @@ export default Ember.Component.extend({
   trail: '',
   duration: null,
   note: '',
-  user_id: function () {
+  user_id: function() {
     return this.get('auth').get('credentials.id')
   },
-  mountain_id: function () {
+  mountain_id: function() {
     this.get('id')
   },
-  hike: Ember.computed('when', 'trail', 'duration', 'note', 'mountain_id', 'user_id', function () {
+  hike: Ember.computed('when', 'trail', 'duration', 'note', 'mountain_id', 'user_id', function() {
     return {
       when: this.get('when'),
       trail: this.get('trail'),
       duration: this.get('duration'),
       note: this.get('note'),
-      // user_id: this.get('user_id'),
       mountain_id: this.get('id')
     }
   }),
-  // hike: {
-  //   when: "2016-05-07",
-  //   trail: "Falls",
-  //   duration: 2,
-  //   note: "Test Test!",
-  //   user_id: 1,
-  //   mountain_id: 3
-  // },
+
   actions: {
-  createHike () {
-    console.log(this.get('hike'));
-    this.get('store').createRecord('hike', this.get('hike')).save().then(() => {
-      console.log('saved?')
-    })
-    // .then(
-      // () => {
-      //   this.get('router').transitionTo('mountains')
-      // }
-    // )
-  },
- }
+    createHike() {
+      this.get('store').createRecord('hike', this.get('hike')).save()
+        .then(() => {
+          console.log('saved')
+          this.get('router').transitionTo('mountains')
+        })
+        .catch((error) => {
+          this.get('flashMessages')
+            .danger('Problem adding your hike. Please try again.');
+        })
+    }
+  }
 });
--- app/components/hike-form-edit.js
+++ app/components/hike-form-edit.js
@@ -1,3 +1,5 @@
+import Ember from 'ember';
+
 export default Ember.Component.extend({
   positionalParams: ['id'],
   auth: Ember.inject.service(),
@@ -8,48 +10,39 @@ export default Ember.Component.extend({
   trail: '',
   duration: null,
   note: '',
-  user_id: function () {
+  user_id: function() {
     return this.get('auth').get('credentials.id')
   },
-  mountain_id: function () {
+  mountain_id: function() {
     this.get('id')
   },
-  hike: Ember.computed('when', 'trail', 'duration', 'note', 'mountain_id', 'user_id', function () {
+  hike: Ember.computed('when', 'trail', 'duration', 'note', 'mountain_id', 'user_id', function() {
     return {
       when: this.get('when'),
       trail: this.get('trail'),
       duration: this.get('duration'),
       note: this.get('note'),
-      // user_id: this.get('user_id'),
       mountain_id: this.get('id')
     }
   }),
-  // hike: {
-  //   when: "2016-05-07",
-  //   trail: "Falls",
-  //   duration: 2,
-  //   note: "Test Test!",
-  //   user_id: 1,
-  //   mountain_id: 3
-  // },
+
   actions: {
-  updateHike () {
-    console.log(this.get('hike'));
-    this.get('store').findRecord('hike', this.get('id')).then(
+    updateHike() {
+      this.get('store').findRecord('hike', this.get('id')).then(
         (record) => {
-          console.log(record)
           record.set('when', this.get('hike').when);
           record.set('trail', this.get('hike').trail);
           record.set('duration', this.get('hike').duration);
           record.set('note', this.get('hike').note);
-          record.save();
-        }
-      )
-    // .then(
-      // () => {
-      //   this.get('router').transitionTo('mountains')
-      // }
-    // )
-  },
- }
+          record.save()
+            .then(() => {
+              this.get('router').transitionTo('mountains')
+            })
+            .catch((error) => {
+              this.get('flashMessages')
+                .danger('Problem editing your hike. Please try again.');
+            })
+        })
+    }
+  }
 });
