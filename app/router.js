84e88acc0d7440496ea622622b28c4008265fb9e
RoryEnright <enright2062@gmail.com>
2017-07-25 01:48:26 -0400

    Fix app router to working set up from cleanup branch

--- app/router.js
+++ app/router.js
@@ -10,17 +10,11 @@ Router.map(function () {
   this.route('sign-in');
   this.route('change-password');
   this.route('mountains');
-<<<<<<< HEAD
-  this.route('mountain', { path: 'mountains/:mountain_id'});
-  this.route('hikes');
-  this.route('hike', {path: 'hikes/:hike_id'});
-=======
   this.route('mountain', { path: 'mountains/:mountain_id'}, function() {
     this.route('hikes');
   });
   this.route('hike-edit',{ path: 'hikes/edit/:id'}); //id = hike id
   this.route('hike-add', { path: 'hikes/add/:id'}); //id = mountain id
->>>>>>> cleanup
 });
 
 export default Router;
