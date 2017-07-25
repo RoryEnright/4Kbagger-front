a0726e62d7a9763ebdd186e15008cae62dc056bc
RoryEnright <enright2062@gmail.com>
2017-07-25 15:48:33 -0400

    Setting up deployment

--- config/environment.js
+++ config/environment.js
@@ -1,12 +1,12 @@
 /* jshint node: true */
-
+'use strict';
 module.exports = function (environment) {
-  'use strict';
-  const ENV = {
+  var ENV = {
     modulePrefix: 'four-k-bagger',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +49,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/4Kbagger-front';
     ENV.locationType = 'hash';
+    ENV.apiHost = 'https://glacial-wildwood-35738.herokuapp.com/'
   }
 
   return ENV;
